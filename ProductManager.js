a7df602b5aed1066e35afd315971ee82786385bf
damarislc <damarislc9@gmail.com>
2024-01-05 15:47:57 -0500

    Implementación de manejo de archivos para la clase ProductManager

--- ProductManager.js
+++ ProductManager.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 /**
  * Clase ProductManager
  * Contiene un arreglo de objetos.
@@ -6,84 +8,180 @@
  * *addProduct - Añade productos al arreglo, recibe un objeto
  * *getProduct - Devuelve el arreglo de objetos
  * *getProductById - Devuelve el objeto según su Id
+ * *updateProduct - Actualiza el objeto con los datos de otro objeto
  */
 class ProductManager {
   /**
-   * Constructor que inicializa el arreglo vacío
+   * Constructor que inicializa el arreglo vacío y el path del json
    */
   constructor() {
     this.products = [];
+    this.path = "productos.json";
   }
 
   /**
-   * Método addProduct
+   * Método addProduct, manda a llamar el método getProducts para obtener los productos
+   * desde el JSON y buscar si ya existe el producto, si ya existe, no lo agrega.
+   * Si no existe, lo añade al arreglo y el archivo JSON.
    * @param {*} es un objeto desestructurado
    * @returns none
    */
-  addProduct({ title, description, price, thumbnail, code, stock }) {
-    if (this.products.some((product) => product.code === code)) {
+  //addProduct({ title, description, price, thumbnail, code, stock }) {
+  addProduct(newProduct) {
+    //Obtiene todos los productos desde el JSON y los añade al arreglo
+    this.getProducts();
+    /*Si el codigo del producto y titulo ya existen, manda mensaje de que el producto ya existe 
+    y no lo agrega */
+    if (
+      this.products.some((product) => product.code === newProduct.code) &&
+      this.products.some((product) => product.title === newProduct.title)
+    ) {
       console.log(
-        `El codigo "${code}" ya existe, favor de cambiarlo en el producto "${title}".`
+        `El producto "${newProduct.title}" ya existe, no se agregará a la lista.`
       );
       return;
     }
-    if (!(title && description && price && thumbnail && code && stock)) {
-      console.log(`Favor de llenar todos los campos en el producto "${title}"`);
+    //Si el codigo del producto ya existe manda un mensaje para que se cambie el código.
+    if (this.products.some((product) => product.code === newProduct.code)) {
+      console.log(
+        `El codigo "${newProduct.code}" ya existe, favor de cambiarlo en el producto "${newProduct.title}".`
+      );
+      return;
+    }
+    //Si hay campos vacíos, manda un mensaje para que se completen todos los campos.
+    if (
+      !(
+        newProduct.title &&
+        newProduct.description &&
+        newProduct.price &&
+        newProduct.thumbnail &&
+        newProduct.code &&
+        newProduct.stock
+      )
+    ) {
+      console.log(
+        `Favor de llenar todos los campos en el producto "${newProduct.title}"`
+      );
       return;
     }
 
-    const product = {
-      id: ProductManager.setId(),
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock,
-    };
+    //Crea el id para el nuevo producto
+    const id = this.setId();
+    //añade el nuevo producto con el id al arreglo
+    this.products.push({ id, ...newProduct });
 
-    this.products.push(product);
+    //Crea al archivo JSON con el contenido del arreglo.
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(this.products));
+      console.log("Datos guardados correctamente en el archivo", this.path);
+    } catch (error) {
+      console.error("Error al escribir en el archivo", error);
+    }
   }
 
   /**
-   * Método getProducts para obtener el arreglo de todos los productos
+   * Método getProducts para obtener el arreglo de todos los productos,
+   * lee el archivo json y luego parsea los datos al arreglo.
    * @returns el arreglo de productos
    */
   getProducts() {
+    try {
+      //Lee el archivo JSON
+      const data = fs.readFileSync(this.path);
+      //Parsea el contenido y lo asigna al arreglo de la clase
+      this.products = JSON.parse(data);
+      console.log("Archivo leido exitosamente.");
+    } catch (error) {
+      console.error("Error al leer el archivo,");
+      //Si da un error -4058 es que el archivo no existe
+      if (error.errno === -4058) console.error("El archivo no existe.");
+      //cualquier otro error, muestra el mensaje de dicho error.
+      else console.error(error);
+    }
+
     return this.products;
   }
 
   /**
-   * Método getProductById para obtener el producto según su id
+   * Método getProductById para obtener el producto según su id. Obtiene primero todos los productos
+   * para popular el arreglo desde el archivo y luego buscar el objeto por el id.
    * @param {*} id
    * @returns regresa el producto si lo encuentra o un mensaje de que no lo encontró.
    */
   getProductById(id) {
+    //Obtiene todos los productos desde el JSON y los añade al arreglo
+    this.getProducts();
+    //Busca el objeto segun el id
     const product = this.products.find((product) => product.id === id);
+    //si el producto es undefined es que no existe y duelve un mensaje, sino regresa el objeto.
     return product === undefined
       ? `El producto con el id ${id} no existe`
       : product;
   }
 
   /**
-   * Método estático para crear el id autoincremental.
-   * @returns
+   * Método para actualizar un objeto
+   * @param {*} id el id del objeto a actualizar
+   * @param {*} productUpdated el objeto con el/los campos modificados
+   * @returns none
    */
-  static setId() {
-    //return (this.lastId = !this.lastId ? 1 : this.lastId++);
-    if (!this.lastId) this.lastId = 1;
+  updateProduct(id, productUpdated) {
+    //Obtiene todos los productos desde el JSON y los añade al arreglo
+    this.getProducts();
+
+    //Si el id no existe, muestra un mensaje de error y se sale del método
+    if (this.products.find((product) => product.id === id) === undefined) {
+      console.error(
+        `El id ${id} no existe, no se puede actualizar el producto.`
+      );
+      return;
+    }
+
+    //Obtiene el index en el array del objeto con el id a actualizar
+    const objIndex = this.products.findIndex((product) => product.id === id);
+    //Cambia el objeto en el índice encontrado por el nuevo objeto con el id que tenía.
+    this.products[objIndex] = { id, ...productUpdated };
+
+    //Reescribe el archivo con el objeto actualizado
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(this.products));
+      console.log("Datos actualizados correctamente en el archivo", this.path);
+    } catch (error) {
+      console.error("Error al actualizar en el archivo", error);
+    }
+  }
+
+  /**
+   * Método para crear el id autoincremental.
+   * @returns el nuevo id para el objeto.
+   */
+  setId() {
+    this.lastId = this.getLastProductId();
+    if (this.lastId === 0) this.lastId = 1;
     else this.lastId++;
     return this.lastId;
+  }
+
+  /**
+   * Método para obtener el último id del arreglo, si el arreglo no
+   * tiene ningún objeto, devuelve 0.
+   * @returns el último id de product o 0
+   */
+  getLastProductId() {
+    if (this.products.length === 0) return 0;
+    const lastProductId = this.products[this.products.length - 1].id;
+    console.log("Last product id=", lastProductId);
+    return lastProductId;
   }
 }
 
 /************************ TESTS *****************************/
 
 //Los tests que utilicé para probar el código, descomentar si se quieren reutilizar.
-/*
+
 const productM = new ProductManager();
 console.log(productM.getProducts());
-product1 = {
+const product1 = {
   title: "Mi producto 1",
   description: "Descripcion",
   price: "123",
@@ -91,15 +189,20 @@ product1 = {
   code: "abc",
   stock: 5,
 };
-product2 = {
+productM.addProduct(product1);
+
+const product2 = {
   title: "Mi producto 2",
   description: "Descripcion",
   price: "456",
   thumbnail: "mi imagen",
-  code: "abc",
+  code: "123",
   stock: 3,
 };
-product3 = {
+
+productM.addProduct(product2);
+
+const product3 = {
   title: "Mi producto 3",
   description: "Descripcion",
   price: "789",
@@ -107,21 +210,14 @@ product3 = {
   code: "def",
   stock: 10,
 };
-product4 = {
-  title: "Mi producto 4",
-  description: "Descripcion",
-  price: "789",
-  thumbnail: "mi imagen",
-  //code: "def",
-  stock: 10,
-};
-productM.addProduct(product1);
-productM.addProduct(product2);
+
 productM.addProduct(product3);
-productM.addProduct(product4);
-console.log(productM.getProducts());
+
+product3.title = "Este es el nuevo titulo del producto";
+productM.updateProduct(3, product3);
+
+console.log("Los productos son:", productM.getProducts());
 
 console.log("****Product by id******");
 console.log(productM.getProductById(2));
 console.log(productM.getProductById(5));
-*/
